8b0d4e33af8f6860cc67dc1c9a9e820c3df0578c
werniq <qniwwwersss@gmail.com>
2024-11-12 16:56:54 +0100

    Added routing module and Updated home component to retrieve data about tasks

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
-import {NavbarComponent} from '../navbar/navbar.component';
+import { Component, OnInit } from '@angular/core';
+import axios from "axios";
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    NavbarComponent
-  ],
+  imports: [NavbarComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  data: { id: number; title: string; description: string; credits: number; created_at: string }[] = [];
 
+  ngOnInit(): void {
+    this.fetchTasks();
+  }
+
+  fetchTasks() {
+    axios.get("https://localhost:8080/tasks")
+      .then((response) => {
+        this.data = response;
+      }, (error) => {
+        console.error("Error fetching data: ", error)
+      })
+  }
 }
