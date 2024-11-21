c941603efc0621c462f58ed9762c3f6bb9c12fab
werniq <qniwwwersss@gmail.com>
2024-11-21 14:18:46 +0100

    Updated tasks component to contain a link to detailed task view

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import axios, { AxiosResponse } from "axios";
 import { NavbarComponent } from '../navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, NgForOf],
+  imports: [NavbarComponent, NgForOf, RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
@@ -14,30 +15,35 @@ import {NgForOf} from '@angular/common';
 export class HomeComponent implements OnInit {
   data: any = [
     {
+      id: 1,
       title: 'Design a Landing Page',
       description: 'Create a modern landing page for a tech startup. Use responsive design principles and ensure compatibility across devices.',
       credits: 50,
       created_at: '2024-11-01'
     },
     {
+      id: 2,
       title: 'Build a REST API',
       description: 'Develop a RESTful API for managing a to-do list. Include CRUD operations and JWT authentication.',
       credits: 75,
       created_at: '2024-11-05'
     },
     {
+      id: 3,
       title: 'Write SEO Articles',
       description: 'Write three SEO-friendly articles on digital marketing trends and tips. Each article should be at least 1,000 words.',
       credits: 30,
       created_at: '2024-11-10'
     },
     {
+      id: 4,
       title: 'Implement E-commerce Cart',
       description: 'Implement a shopping cart feature for an e-commerce site, including add, remove, and checkout functionalities.',
       credits: 90,
       created_at: '2024-11-07'
     },
     {
+      id: 5,
       title: 'Develop Chatbot Functionality',
       description: 'Build a chatbot using AI that assists users with frequently asked questions and guides them through the site.',
       credits: 100,
@@ -50,7 +56,7 @@ export class HomeComponent implements OnInit {
   }
 
   fetchTasks() {
-    axios.get("http://localhost:8080/tasks")
+    axios.get("http://localhost:8080/api/v1/tasks")
       .then((response) => {
         this.data = response.data;
       })
