7131b03ae187cb24a70ffb76213954982481fde3
werniq <qniwwwersss@gmail.com>
2024-11-21 14:28:20 +0100

    Updated home component; Check user type and display related home component

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ClientHomeComponent } from '../client-home/client-home.component';
+import { FreelancerHomeComponent } from '../freelancer-home/freelancer-home.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ClientHomeComponent, FreelancerHomeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  userType = localStorage.getItem('userType') || "freelancer";
 }
