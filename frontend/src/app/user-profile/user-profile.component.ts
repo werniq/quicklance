3bc9727e255932e1e01af9ff3e0f36f0532a522f
Oleksand Matviienko <qniwwwersss@gmail.com>
2025-01-11 11:23:10 +0100

    feat: Implemented userprofile component; added functionality to udpateUserBalance

--- frontend/src/app/user-profile/user-profile.component.ts
+++ frontend/src/app/user-profile/user-profile.component.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
+import {FormsModule} from '@angular/forms';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgIf
+  ],
   templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.css'
+  styleUrls: ['./user-profile.component.css']
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+  user: { id: number; firstname: string; lastname: string; type: string; credits: number } | null = null;
+  isAdmin: boolean = false;
+  creditsToUpdate: number = 0;
+  successfulMessage: string = "";
 
+  ngOnInit(): void {
+    this.getUserData();
+    this.checkIfAdmin();
+  }
+
+  getUserData() {
+    const userId = localStorage.getItem('userId');
+    if (!userId) {
+      console.error('User ID not found in localStorage');
+      return;
+    }
+
+    axios
+      .get(`http://localhost:8080/api/v1/user/${userId}`)
+      .then(response => {
+        this.user = response.data;
+      })
+      .catch(error => {
+        console.error('Error fetching user data:', error);
+      });
+  }
+
+  checkIfAdmin() {
+    const userId = localStorage.getItem('userId');
+    this.isAdmin =  userId == this.user?.id.toString();
+  }
+
+  updateUserCredits() {
+    if (!this.user) return;
+
+    axios
+      .post('http://localhost:8080/api/v1/user/credits', {
+        userId: this.user.id,
+        credits: this.creditsToUpdate
+      })
+      .then(response => {
+        this.successfulMessage = response.data.message;
+        if (this.user) this.user.credits = this.creditsToUpdate;
+      })
+      .catch(error => {
+        console.error('Error updating user credits:', error);
+        alert('Failed to update user credits');
+      });
+  }
 }
